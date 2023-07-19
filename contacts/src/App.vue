79914473a3b59336efb144e4e9ea8ecf46612ae3
Caio César Beleza <caio.almeidabeleza@gmail.com>
2023-07-19 10:51:10 -0300

    Initiating project

---
+++ contacts/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <NavBar/>
+  <Spinner/>
+  <router-view/>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+  export default {
+    name: 'App',
+    components: {NavBar}
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ contacts/src/views/ContactManager.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <p class="h3 text-success fw-bold">Contact Manager
+                    <router-link to="/contacts/add" class="btn btn-success btn-sm">New</router-link>
+                </p>
+                <form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                <input type="text" class="form-control" placeholder="Search Name">
+                                </div>
+                                <div class="col">
+                                    <input type="submit" class="btn btn-outline-dark">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default{
+    name: "ContactManager"
+}
+
+</script>
+
+<style>
+
+</style>
